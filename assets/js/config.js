cc552b28e90e5405e47ed2ed6735d64fdb8cf767
ojolmolisbersatu-id <haybaonline@gmail.com>
2026-09-24 09:47:46 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,7 @@
-window.OMB_CONFIG={
-  SUPABASE_URL:"https://igcqmowxkrnhwtlvmdfk.supabase.co",
-  SUPABASE_ANON_KEY:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJpZ2NxcW13eGtybmh3dGx2bWRmaIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk4ODA2NDIsImV4cCI6MjEwNTQ1NjY0Mn0.ZgtT5tvNJiQUTpNAoOngf0KDmZTSXBt85Ql49TPlknQ", 
-  FUNCTIONS_BASE:"https://igcqmowxkrnhwtlvmdfk.supabase.co/functions/v1"
+window.OMB_CONFIG = {
+  SUPABASE_URL: "https://igcqmowxkrnhwtlvmdfk.supabase.co",
+
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlnY3Ftb3d4a3JuaHd0bHZtZGZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk4ODA2NDIsImV4cCI6MjEwNTQ1NjY0Mn0.ZgtT5tvNJiQUTpNAoOngf0KDmZTSXBt85Ql49TPlknQ",
+
+  FUNCTIONS_BASE: "https://igcqmowxkrnhwtlvmdfk.supabase.co/functions/v1"
 };
